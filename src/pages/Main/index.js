fe4ae64aa3ca1e9b62ce91f1ae077943b14b573c
Jaderson Aires <jadersonaires@gmail.com>
2021-03-04 23:35:11 -0300

    add header logo and name app

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
 import { Container } from './styles'
+
+import Header from '../../components/Header'
 
 export default function Main() {
   return (
     <Container>
-      <Text>Hollo word!</Text>
+      <Header />
     </Container>
   );
 }
