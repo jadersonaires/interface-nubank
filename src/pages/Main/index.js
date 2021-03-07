c3e0dc13c3871bccfda7e0ef299f4397077c1c24
Jaderson Aires <jadersonaires@gmail.com>
2021-03-07 20:12:42 -0300

    create componente tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,11 +3,13 @@ import {Text} from 'react-native';
 import { Container } from './styles'
 
 import Header from '../../components/Header'
+import Tabs from '../../components/Tabs'
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Tabs />
     </Container>
   );
 }
