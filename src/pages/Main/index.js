d808bddbbb5ad3aef7a0f09ed4c12062101314d4
Jaderson Aires <jadersonaires@gmail.com>
2021-03-07 21:27:55 -0300

    View mean app with styled

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import {Text} from 'react-native';
-import { Container } from './styles'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation} from './styles'
 
 import Header from '../../components/Header'
 import Tabs from '../../components/Tabs'
@@ -9,6 +18,25 @@ export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+         <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+         </CardHeader>
+         <CardContent>
+           <Title>Salto disponível</Title>
+           <Description>R$ 210.456,01</Description>
+         </CardContent>
+         <CardFooter>
+           <Annotation>
+             Transferência feita para Banco do Brasil 300,00 as 19:00 horas.
+           </Annotation>
+         </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
